f011f1b2b4a0a3167ccd5d02ba7da42697d7804e
740291272 <740291272@qq.com>
2019-04-15 22:24:25 +0800

    bug fix

--- dmsl-runner/main.py
+++ dmsl-runner/main.py
@@ -3,39 +3,37 @@ import sys
 import urllib.request
 import urllib.parse
 import json
-import base64
 import time
 
 def json_paser(injson):
     injson = injson.replace('*s', ' ')
     injson = injson.replace('*y', '"')
     jsonin = json.loads(injson)
-    if jsonin["language"] is "dmsl":
+    if jsonin["language"] == "dmsl":
         code = jsonin["code"]
         return code
+    else:
+        print("json error")
 
 
 def dmsl_runner(code):
-    temp = str(time.time()).replace(".", "a")[0: 6]
-    ok_dir = os.mkdir("/tmp/" + temp)
-    if ok_dir is True:
-        path = "/tmp/" + temp + "main.dmsl"
-        f = open(path, "w+")
-        f.write(code)
-        f.close
-        outstd = os.pipe("DmslRunner " + path)
-        print(outstd)
-        return outstd
+    temp = str(time.time()).replace(".", "a")[0: 10]
+    os.mkdir("/tmp/" + temp)
+    path = "/tmp/" + temp + "main.dmsl"
+    f = open(path, "w+")
+    f.write(code)
+    outstd = os.popen("DmslRunner " + path).read()
+    return outstd
 
 
-def back_post(str, address):
+def back_post(str_in, address):
     url = str(address)
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;Win64;x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36',
         'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
     }
     values = {
-        'stdout': str
+        'stdout': str_in
     }
     data = urllib.parse.urlencode(values).encode('utf-8')
     request = urllib.request.Request(url, data, headers)
@@ -46,7 +44,7 @@ def back_post(str, address):
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Arg Error")
-    else:    
+    else:
         instr = sys.argv[1]
         address = sys.argv[2]
         sta1 = json_paser(instr)

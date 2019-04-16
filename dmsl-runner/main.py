8b0dcd66083da17a517ba6ea9bd20a15cf78f0de
740291272 <740291272@qq.com>
2019-04-16 10:59:38 +0800

    bug fix

--- dmsl-runner/main.py
+++ dmsl-runner/main.py
@@ -3,19 +3,18 @@ import sys
 import urllib.request
 import urllib.parse
 import json
-import time
+import base64
 
 
 DEBUG = 0
 
 
 def json_paser(injson):
-    injson = injson.replace('*s', ' ')
-    injson = injson.replace('*y', '"')
+    injson = base64.decode(injson)
     jsonin = json.loads(injson)
     if jsonin["language"] == "dmsl":
         code = jsonin["code"]
-        code = str(code).replace('*n', '\n')
+        code = base64.decode(code)
         if DEBUG == 1:
             print(code)
         return code
@@ -38,30 +37,14 @@ def dmsl_runner(code):
     return outstd
 
 
-def back_post(str_in, address):
-    url = str(address)
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;Win64;x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36',
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-    }
-    values = {
-        'stdout': str_in
-    }
-    data = urllib.parse.urlencode(values).encode('utf-8')
-    request = urllib.request.Request(url, data, headers)
-    output = urllib.request.urlopen(request).read().decode('utf-8')
-    return output
-
-
 if __name__ == "__main__":
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 2:
         print("Arg Error")
     else:
         instr = sys.argv[1]
-        address = sys.argv[2]
-        if len(sys.argv) == 4:
-            DEBUG = sys.argv[3]
+        if len(sys.argv) == 3:
+            DEBUG = sys.argv[2]
         sta1 = json_paser(instr)
         sta2 = dmsl_runner(sta1)
         sta2 = sta2.read()
-        back_post(sta2, address)
+        print(sta2)

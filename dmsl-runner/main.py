8a3fd72737201c45953ab19f4277b5aedf3346a5
740291272 <740291272@qq.com>
2019-04-16 14:04:46 +0800

    完成啦！！！！

--- dmsl-runner/main.py
+++ dmsl-runner/main.py
@@ -1,21 +1,21 @@
 import os
 import sys
-import urllib.request
-import urllib.parse
 import json
 import time
 import base64
+import subprocess
 
 
 DEBUG = 0
 
 
 def json_paser(injson):
+    injson = str.encode(injson)
     injson = base64.b64decode(injson)
     jsonin = json.loads(injson)
     if jsonin["language"] == "dmsl":
         code = jsonin["code"]
-        code = base64.b64decode(code)
+        code = bytes.decode(base64.b64decode(code))
         if DEBUG == 1:
             print(code)
         return code
@@ -24,18 +24,25 @@ def json_paser(injson):
 
 
 def dmsl_runner(code):
-    temp = str(time.time()).replace(".", "a")[0: 9]
+    temp = str(time.time()).replace(".", "a")[0: 13]
     os.mkdir("/tmp/" + temp)
-    path = "/tmp/" + temp + "/main.dmsl"
-    f = open(path, "w+")
+    path = "/tmp/" + temp
+    f = open(path + "/demo.dmsl", "w+")
     f.write(code)
-    run_path = "DmslRunner " + path
-    outstd = os.popen(run_path)
+    f.close()
+    run_path = "cd " + path + " && DmslRunner demo.dmsl"
+
+    outstd = subprocess.Popen(run_path, shell=True, stdout=subprocess.PIPE)
+    outstd.wait()
+
     if DEBUG == 1:
         print(path)
         print(run_path)
         print(outstd)
-    return outstd
+        pass
+    out = outstd.stdout.read()
+    out = bytes.decode(out)
+    return out
 
 
 if __name__ == "__main__":
@@ -43,9 +50,6 @@ if __name__ == "__main__":
         print("Arg Error")
     else:
         instr = sys.argv[1]
-        if len(sys.argv) == 3:
-            DEBUG = sys.argv[2]
         sta1 = json_paser(instr)
         sta2 = dmsl_runner(sta1)
-        sta2 = sta2.read()
         print(sta2)
